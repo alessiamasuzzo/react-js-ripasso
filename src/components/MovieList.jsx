63a88dbf5e3ea0e2a6c4c96baffc0af8644f4281
alessiamasuzzo <alessia.masuzzo@edu-its.it>
2026-09-07 11:01:25 +0000

    wip: second exercise - array, object

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,6 @@
+export default function MovieList({movies}){
+    return(
+        <>
+        </>
+    )
+}
---
+++ src/data/movies.js
@@ -0,0 +1,25 @@
+movies = [{
+    id: 1,
+    title: "Le pagine della nostra vita",
+    year: 2004,
+    poster: "https://image.tmdb.org/t/p/original/2ftj0Q2r8WnwFPEdrxeL7x8NNzq.jpg",
+    genre: "Romantico",
+    watched: true,
+},
+{
+    id: 2,
+    title: "Harry Potter e il prigioniero di Azkaban",
+    year: 2004,
+    poster: "https://www.odeonline.it/app/uploads/2024/05/7a9e7935-a7e3-4856-9466-8852ae70277c.jpg",
+    genre: "Fantasia",
+    watched: true,
+},
+{
+    id: 3,
+    title: "Il principe e il pirata",
+    year: 2001,
+    poster: "https://pad.mymovies.it/filmclub/2001/12/005/imm.jpg",
+    genre: "Fantasia",
+    watched: false,
+},
+]
